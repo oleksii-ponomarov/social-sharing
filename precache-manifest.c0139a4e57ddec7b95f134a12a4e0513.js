85c64e2243afb57475c012bf60809dde934f169c
Oleksii Ponomarov <rocknrollerr@gmail.com>
2020-04-20 21:41:43 +0300

    Updates

---
+++ precache-manifest.c0139a4e57ddec7b95f134a12a4e0513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3daf6593ffb1387b1dd08f62288dd4",
+    "url": "/social-sharing/index.html"
+  },
+  {
+    "revision": "7db5036b23c1053ce4cc",
+    "url": "/social-sharing/static/css/main.5f73fd77.chunk.css"
+  },
+  {
+    "revision": "abb1638d815fa94deac3",
+    "url": "/social-sharing/static/js/2.be8ec7a4.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/social-sharing/static/js/2.be8ec7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db5036b23c1053ce4cc",
+    "url": "/social-sharing/static/js/main.39e58506.chunk.js"
+  },
+  {
+    "revision": "2f5525c6ffc68823e054",
+    "url": "/social-sharing/static/js/runtime-main.5209f53d.js"
+  }
+]);
